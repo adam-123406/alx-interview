eb79d4650fdb27c55540437e0651fab32cd8469a
adam-123406 <adamabdallahus@gmail.com>
2023-08-13 19:51:49 +0000

    minimum

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,6 +3,7 @@
 Minimum Operations
 '''
 
+
 def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
@@ -32,4 +33,4 @@ def minOperations(n):
             ops_count += 1
             # print('-(01)->{}'.format('H' * done), end='')
     # print('')
-    return ops_count
+return ops_count
