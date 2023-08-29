a66e061817d0a8b1510a57707c4a38fca07bf450
adam-123406 <adamabdallahus@gmail.com>
2023-08-29 10:16:46 +0000

    N qeens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-"""N Qeens"""
+""" N queens """
 import sys
 
+
 if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqeen N")
+    print("Usage: nqueens N")
     exit(1)
 
 if not sys.argv[1].isdigit():
-    print(N most be a number)
+    print("N must be a number")
     exit(1)
 
 if int(sys.argv[1]) < 4:
-    print(N must be at least)
+    print("N must be at least 4")
     exit(1)
 
 n = int(sys.argv[1])
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """find possible positions"""
+    """ find possible positions """
     if i < n:
         for j in range(n):
-            if j not in a and i + j not b and i - j not in c:
+            if j not in a and i + j not in b and i - j not in c:
                 yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
 
 
-def slvoe(n):
-    """slvoe"""
-
+def solve(n):
+    """ solve """
     k = []
     i = 0
     for solution in queens(n, 0):
         for s in solution:
             k.append([i, s])
             i += 1
-            print(n)
-            k = []
-            i = 0
+        print(k)
+        k = []
+        i = 0
 
 
 solve(n)
